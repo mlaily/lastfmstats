0664bea4a5d806ee141d01c012ccea3dd8ce1015
Melvyn Laïly <melvyn.laily@gmail.com>
2021-03-12 21:10:22 +0100

    Extract the EF Core data model to a new C# project

--- src/RelationalSchema/MainContext.cs
+++ src/RelationalSchema/MainContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Dapper;
+//using Dapper;
 using System.Runtime.CompilerServices;
 using System.Linq;
 using System.Text;
